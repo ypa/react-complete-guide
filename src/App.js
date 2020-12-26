82da89a61dae17793c5c4a9169ad9405244fef86
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-25 20:10:30 -0800

    Mutate state object by calling setState

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ class App extends Component {
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29},
       { name: 'Stephanie', age: 26 }
-    ]
+    ],
+    otherState: 'some other value'
   }
 
   switchNameHandler = () => {
-    console.log('Was clicked!');
+    // console.log('Was clicked!');
+    // DON'T DO THIS, mutating state directly is bad: this.state.persons[0].name = 'Maximilian'
+    // Instead DO this:
+    this.setState({
+      persons: [
+      { name: 'Maximilian', age: 28 },
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 27 }
+    ]});
   }
 
   render() {
