1f5a3774a633177668d5d824a022e32f07ea85aa
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-29 17:54:44 -0800

    Apply styling with radium's pseudo selectors

--- src/App.js
+++ src/App.js
@@ -48,7 +48,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -68,6 +72,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
