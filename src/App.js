07aad7f71f24f0224f4a24bf187200192cac0b98
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-31 20:12:24 -0800

    Refactor button element to using CSS modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -42,10 +42,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-    };
-
     let persons = null;
+    let btnClass = '';
     
     if (this.state.showPersons) {
       persons = (
@@ -61,26 +59,22 @@ class App extends Component {
           })}
         </div>
       );
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // }
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button className="button" onClick={this.togglePersonsHandler}>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button className={btnClass} onClick={this.togglePersonsHandler}>
             Toggle Persons
           </button>
           {persons}
